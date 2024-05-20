263e1210e2c38f89bcd82d03f4467ce858970607
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-05-20 19:19:38 -0300

    :sparkles: With databases SQLite & SQLAlchemy

--- repository/database.py
+++ repository/database.py
@@ -1,10 +1,43 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Float, create_engine
 from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./satellites.db"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from sqlalchemy.ext.declarative import declarative_base
+from model.data_class import Satellite, Coordinates
 
 Base = declarative_base()
+
+
+class SatelliteDB(Base):
+    __tablename__ = "satellites"
+
+    name = Column(String, primary_key=True)
+    distance = Column(Float)
+    x = Column(Float)
+    y = Column(Float)
+    message = Column(String)
+
+
+def satellite_to_db(satellite):
+    return SatelliteDB(
+        name=satellite.name,
+        distance=satellite.distance,
+        x=satellite.coordinates.x,
+        y=satellite.coordinates.y,
+        message=",".join(satellite.message),
+    )
+
+
+def db_to_satellite(satellite_db):
+    return Satellite(
+        name=satellite_db.name,
+        distance=satellite_db.distance,
+        coordinates=Coordinates(x=satellite_db.x, y=satellite_db.y),
+        message=satellite_db.message.split(","),
+    )
+
+
+# Crear una base de datos SQLite en memoria y una sesión de SQLAlchemy
+engine = create_engine('sqlite:///databases/satellites.db')
+Session = sessionmaker(bind=engine)
+
+# Crear la tabla satellites en la base de datos
+Base.metadata.create_all(engine)
--- services/satellites_services.py
+++ services/satellites_services.py
@@ -1,30 +1,45 @@
+from sqlalchemy.orm import Session
 from model.data_class import Satellite, Coordinates
-
+from repository.database import SatelliteDB, satellite_to_db, db_to_satellite, Session
 
 class SatelliteService(object):
-    satellites: list[Satellite] = []
+    session: Session = Session()
 
     @classmethod
     def add_satellite(cls, satellite: Satellite) -> None:
-        cls.satellites.append(satellite)
+
+        satellite_db = cls.session.query(SatelliteDB).filter(SatelliteDB.name == satellite.name).first()
+        if satellite_db:
+            satellite_db.distance = satellite.distance
+            satellite_db.x = satellite.coordinates.x
+            satellite_db.y = satellite.coordinates.y
+            satellite_db.message = ",".join(satellite.message)
+            cls.session.commit()
+        else:
+            cls.session.add(satellite_db)
+            cls.session.commit()
 
     @classmethod
     def update_satellite(cls, satellite: Satellite) -> None:
-        for i, s in enumerate(cls.satellites):
-            if s.name == satellite.name:
-                cls.satellites[i] = satellite
-                break
+        satellite_db = cls.session.query(SatelliteDB).filter(SatelliteDB.name == satellite.name).first()
+        if satellite_db:
+            satellite_db.distance = satellite.distance
+            satellite_db.x = satellite.coordinates.x
+            satellite_db.y = satellite.coordinates.y
+            satellite_db.message = ",".join(satellite.message)
+            cls.session.commit()
 
     @classmethod
     def get_satellite(cls, satellite_name: str) -> Satellite:
-        for satellite in cls.satellites:
-            if satellite.name == satellite_name:
-                return satellite
+        satellite_db = cls.session.query(SatelliteDB).filter(SatelliteDB.name == satellite_name).first()
+        if satellite_db:
+            return db_to_satellite(satellite_db)
         raise Exception("Satellite not found")
 
     @classmethod
     def get_satellites(cls) -> list[Satellite]:
-        return cls.satellites
+        satellites_db = cls.session.query(SatelliteDB).all()
+        return [db_to_satellite(satellite_db) for satellite_db in satellites_db]
 
     @classmethod
     def get_coordinates(cls, satellites: list[Satellite]) -> Coordinates:
